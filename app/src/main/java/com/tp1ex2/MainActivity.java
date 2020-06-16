c147595a5cdd2bea9ed403c81d664e80e7b3c7f7
Sidi Mohamed <sidimohamedreget92@gmail.com>
2020-06-16 23:01:53 +0000

    Initial commit

---
+++ app/src/main/java/com/tp1ex2/MainActivity.java
@@ -0,0 +1,12 @@
+package com.tp1ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
